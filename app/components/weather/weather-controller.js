77e05df1359b62727a372c3909049d487abc8dcc
joe <loseca@yahoo.com>
2016-05-02 16:46:55 -0600

    p

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -2,6 +2,8 @@ app.controller('WeatherController', function(WeatherService){
 	var wc = this;
 	WeatherService.getWeather().then(function(weather){
 		wc.weather = weather;
+		console.log(wc.weather)
+		
 	})
 	// DO NOT EDIT ABOVE
 	
