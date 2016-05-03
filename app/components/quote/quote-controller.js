b800dc18d127d957e089bb21b77deeacceb94505
joe <loseca@yahoo.com>
2016-05-02 19:30:18 -0600

    p

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -2,6 +2,7 @@ app.controller('QuoteController', function(QuoteService){
 	var qc = this;
 	QuoteService.getQuote().then(function(quote){
 		qc.quote = quote;
+		console.log(qc.quote)
 	})
 	// DO NOT EDIT ABOVE
 	
