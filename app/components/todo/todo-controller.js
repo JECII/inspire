020cbcfca53a012b829dc21bdef2bf4440135618
joe <loseca@yahoo.com>
2016-05-03 21:05:13 -0600

    p

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -1,32 +1,30 @@
 app.controller('TodoController', function (TodoService) {
-	var tc = this;
-	tc.todos = TodoService.getTodos();
-	// DO NOT EDIT ABOVE	
- tc.test = 'some value'
-	tc.todoList =[]
+    var tc = this;
+    tc.todos = TodoService.getTodos();
+    // DO NOT EDIT ABOVE	
+    tc.test = 'some value'
+    tc.todoList = []
 
- 
-tc.todoAdd = function(todo) {
-        tc.todoList.push({todo, done:false});
+
+    tc.todoAdd = function (todo) {
+        tc.todoList.push({ todo, done: false });
         tc.todoInput = '';
     };
 
-    tc.remove = function() { 
+    tc.remove = function () {
         var oldList = tc.todoList;
         tc.todoList = [];
-        angular.forEach(oldList, function(x) {
-            if (!x.done) {tc.todoList.push(x)};
-    
-    
+        angular.forEach(oldList, function (x) {
+            if (!x.done) { tc.todoList.push(x) };
+
+
         });
-    
-    
+
+
     };
-    
-    
-        
-    
-    
-    
-    
+
+
+
+
+
 });
